075a2478b32d36146ddbc2e703b583002b564684
AaronTweeton <aaron@aarontweeton.com>
2023-04-19 17:08:19 -0700

    Converted controller function to class.

--- todo.js
+++ todo.js
@@ -1,34 +1,34 @@
 const TodoList = {
-  controller,
+  controller: class Controller {
+    constructor() {
+      this.todos = [
+        { text: "learn AngularJS", done: true },
+        { text: "build an AngularJS app", done: false },
+      ];
+    }
+
+    addTodo() {
+      this.todos.push({ text: this.todoText, done: false });
+      this.todoText = "";
+    }
+
+    remaining() {
+      var count = 0;
+      this.todos.forEach((todo) => {
+        count += todo.done ? 0 : 1;
+      });
+      return count;
+    }
+
+    archive() {
+      var oldTodos = this.todos;
+      this.todos = [];
+      oldTodos.forEach((todo) => {
+        if (!todo.done) this.todos.push(todo);
+      });
+    }
+  },
   templateUrl: "./todo.template.html",
 };
-
-function controller() {
-  this.todos = [
-    { text: "learn AngularJS", done: true },
-    { text: "build an AngularJS app", done: false },
-  ];
-
-  this.addTodo = () => {
-    this.todos.push({ text: this.todoText, done: false });
-    this.todoText = "";
-  };
-
-  this.remaining = () => {
-    var count = 0;
-    this.todos.forEach((todo) => {
-      count += todo.done ? 0 : 1;
-    });
-    return count;
-  };
-
-  this.archive = () => {
-    var oldTodos = this.todos;
-    this.todos = [];
-    oldTodos.forEach((todo) => {
-      if (!todo.done) this.todos.push(todo);
-    });
-  };
-}
 
 angular.module("todoApp", []).component("todoList", TodoList);
